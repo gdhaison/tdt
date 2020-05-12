1239f3304173d6fbb1024b8fbb0e592b87dfecb7
gdhaison <vuhaisonhb456@gmail.som>
2020-05-12 18:07:13 +0700

    Like

--- apps/posts/forms.py
+++ apps/posts/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Like
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content', 'user', ]
+
+class CreateLikeForm(forms.ModelForm):
+    class Meta:
+        model = Like
+        fields = ['user', 'post', ]
--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,5 +3,6 @@ from apps.posts.views import (ListPostView, CreatePostView)
 
 app_name = 'apps.posts'
 urlpatterns = [
+    path('create-like/', ListPostView.as_view(), name='create-like'),
     path('create-post/', CreatePostView.as_view(), name='create-post')
 ]
