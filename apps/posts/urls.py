490ad38874637456b02b5a665668e43df95ae8c8
gdhaison <vuhaisonhb456@gmail.som>
2020-05-13 15:06:25 +0700

    comment

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.posts.views import (ListPostView, CreatePostView)
+from apps.posts.views import *
 
 app_name = 'apps.posts'
 urlpatterns = [
+    path('create-comment/', ListPostView.as_view(), name='create-comment'),
     path('create-like/', ListPostView.as_view(), name='create-like'),
-    path('create-post/', CreatePostView.as_view(), name='create-post')
+    path('create-post/', CreatePostView.as_view(), name='create-post'),
+    path('delete-post/<post_id>', delete_like, name='delete-like'),
 ]
